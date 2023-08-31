c0fdb9f17e3f7499d35092fcaa900a80cea4ff7b
fernando-bertolo <fernando.bertolo.jr123@gmail.com>
2023-08-31 19:37:21 -0300

    teste

--- api/routes/usuarios/updateUser.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const updateUser = express.Router();
-
-updateUser.put("/update-user", (req, res) => {
-  res.send("Usuário alterado");
-});
-
-module.exports = updateUser;
--- database/index.js
+++
@@ -1,6 +0,0 @@
-(async () => {
-  const database = require("./db");
-  const usuarios = require("./usuarios/usuarios");
-  const veiculos = require("./veiculos/veiculos");
-  await database.sync();
-})();
